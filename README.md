5c277161c051d421f0da382e6c1a31dab663430c
books4thoughtgit <leah.calapan07@outlook.com>
2024-02-02 16:41:44 +0800

    ⬆ Update

---
+++ README.md
@@ -0,0 +1,10 @@
+# BDjs
+- BDFD JavaScript (a.k.a BDJS) is an another script language available in the app (Bot Designer For Discord a.k.a BDFD) which can be used as alternative to BDScript for developing a Discord Bot. Unlike BDScript, BDJS executes a JavaScript code. It uses ECMAScript 2015 (ES6) JavaScript version. BDJS has limited Discord API functions support available.
+- I (books4thoughtgit/aerialasf) is a part of the BDFD community and I thought, "Why is BDjs so trash?". And so I made a Library to make BDjs better.
+# How to Use
+- Get Node.js
+- Go to Terminal
+- Do `npm install discord.js`
+- Do `npm install bdjs-plus`
+# Credits
+- Me (thats all for now)
---
+++ main.js
@@ -0,0 +1,164 @@
+(function (window) {
+    const { Client, Events, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+    const client = new Client();
+
+    let prefix;
+
+    const bdjsPlus = {
+        version: '0.0.1',
+        createBot: function createBot(token, botPrefix) {
+            try {
+            client.once(Events.ClientReady, readyClient => {
+                console.log(`Logged into ${readyClient.user.tag} and ready to run.`);
+            })
+            client.login(token);
+            prefix = botPrefix;
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        intents: function intents(intentsArray) {
+            const discordIntents = [];
+
+            try {
+            intentsArray.forEach(intent => {
+                discordIntents.push(GatewayIntentBits[intent]);
+            });
+
+            client.options.intents = discordIntents;
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        }, // Objects
+        ctx: {
+            message: message = (item) => {
+                const message = {
+                    content: message.content,
+                    arguments: message.content.slice(prefix.length).trim().split(/ +/),
+                    trigger: message.content.slice(prefix.length).trim().split(/ +/)[0]
+                };
+                
+                return message[item];
+            }
+        },
+        setResponse: function setResponse(messageContent) {
+            let channelId = client.channels.cache.get('channel_id');
+
+            try {
+            channelId.send(messageContent);
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        setEmbedResponse: function setEmbedResponse(color, title, author, authorIcon, addField, inline, fieldValue, footer) {
+            try {
+            let embed = new EmbedBuilder()
+                .setColor(color)
+                .setAuthor({ name: title, iconUrl: authorIcon })
+                .setDescription(author)
+                .addFields({ name: addField, value: fieldValue, inline: inline })
+                .setFooter({ text: footer });
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        ban: function ban(userId) {
+            let user = userId;
+
+            try {
+            guild.members.ban(user);
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        unban: function unban(userId) {
+            let user = userId;
+
+            try {
+            guild.members.unban(user);
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        banWithReason: function banWithReason(userId, reason) {
+            let user = userId;
+            let reason = reason;
+
+            try {
+            if (user) {
+                user.ban({ reason: reason});
+            }
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        kick: function kick(userId) {
+            let user = userId;
+            try {
+            if (user) {
+                user.kick();
+            }
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        kickWithReason: function banWithReason(userId, reason) {
+            let user = userId;
+            let reason = reason;
+
+            try {
+            if (user) {
+                user.kick(reason);
+            }
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        botTyping: function botTyping() {
+            message.channel.sendTyping();
+        },
+        createChannel: function createChannel(name, type) {
+            let channelName = name;
+            let channelType = type;
+            
+            try {
+            function compareTextVoice(item1, item2) {
+                if (item1 === item2) {
+                    if (item2 == 'text') {
+                        return 'GUILD_TEXT';
+                    } else if (item2 == 'voice') {
+                        return 'GUILD_VOICE';
+                    }
+                }
+            }
+
+            let usableChannelType = compareTextVoice(channelType, 'text');
+
+            if (usableChannelType == undefined || null) {
+                let usableChannelType = compareTextVoice(channelType, 'voice');
+            }
+
+            if (channelName && usableChannelType) {
+                guild.channels.create();
+            }
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+        removeChannel: function removeChannel(channelId) {
+            try {
+                let channel = channelId;
+                
+                channel.delete();
+            } catch {
+                console.error(`BDJS: ${error.type} ${error.name}`);
+            }
+        },
+    }
+    // Expose the library to the global scope
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = bdjsPlus;
+    } else {
+        window.bdjsPlus = bdjsPlus;
+    }
+})(window || global);
